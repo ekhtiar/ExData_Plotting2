5f1f2f094c95c5e8ad054caeed8d5f9ef45b4297
Ekhtiar Syed <ekhtiar.syed@gmail.com>
2015-09-27 02:55:40 +0200

    fixed a big bug in Plot1.R

--- Plot2.R
+++ Plot2.R
@@ -6,7 +6,7 @@ years <- c(1999,2002,2005,2008)
 result <- data.frame(Year = integer(), type = character(), Pollutant = character(), Emissions = integer() )
 #run two for loops and subset, the first for loop subsets year and the second for loop subsets that year further by different type
 for (i in years ) {
-  #subset the data by year
+  #subset the data by year and area code
   NEIbyYear <- NEI[NEI$year == i, ]
   for(j in unique(NEIbyYear$type)) {
     #subset further by type
@@ -20,9 +20,9 @@ for (i in years ) {
     result <- rbind(result,buffer)
   }
   #defining the png output
-  png(filename = "./Plot1.png", width = 480, height = 480)
+  #png(filename = "./Plot1.png", width = 480, height = 480)
   #generating the barplot
   barplot(result$Emissions, names.arg = result$Year, beside = TRUE, col= c("red", "green", "black", "yellow"), legend = levels(unique(result$type)),  xlab = "years", ylab = "Emission", main = "Fine Particular Matter Air Pollutant in USA by Years")
   #mandatory housekeeping 
-  dev.off()
+  #dev.off()
 }
