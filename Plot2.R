bf6883a5afdbe29ed46380f5f36da4472713f009
Ekhtiar Syed <ekhtiar.syed@gmail.com>
2015-09-28 00:43:56 +0200

    Plot2.R update

--- Plot2.R
+++ Plot2.R
@@ -4,25 +4,27 @@ NEI <- readRDS("./data/summarySCC_PM25.rds")
 years <- c(1999,2002,2005,2008)
 #create empty matrix to store result
 result <- data.frame(Year = integer(), type = character(), Pollutant = character(), Emissions = integer() )
+#subset the data for Baltimore City, Maryland (fips == "24510")
+NEIbyArea <- NEI[NEI$fips == "24510", ]
 #run two for loops and subset, the first for loop subsets year and the second for loop subsets that year further by different type
 for (i in years ) {
-  #subset the data by year and area code
-  NEIbyYear <- NEI[NEI$year == i, ]
-  for(j in unique(NEIbyYear$type)) {
+  #subset the data by year
+  NEIbyYrNdArea <- NEIbyArea[NEIbyArea$year == i, ]
+  for(j in unique(NEIbyYrNdArea$type)) {
     #subset further by type
-    NEIbyYearandType <- NEIbyYear[NEIbyYear$type == j, ]
+    NEIbyYrNdAreaNdType <- NEIbyYrNdArea[NEIbyYrNdArea$type == j, ]
     #store summary of subset in buffer
     buffer <- data.frame(Year = i, 
                          type = j, 
                          Pollutant = "PM25-PRI", 
-                         Emissions = mean(NEIbyYearandType$Emissions, na.rm = TRUE) )
+                         Emissions = mean(NEIbyYrNdAreaNdType$Emissions, na.rm = TRUE) )
     #store result
     result <- rbind(result,buffer)
   }
   #defining the png output
-  #png(filename = "./Plot1.png", width = 480, height = 480)
+  png(filename = "./Plot2.png", width = 480, height = 480)
   #generating the barplot
-  barplot(result$Emissions, names.arg = result$Year, beside = TRUE, col= c("red", "green", "black", "yellow"), legend = levels(unique(result$type)),  xlab = "years", ylab = "Emission", main = "Fine Particular Matter Air Pollutant in USA by Years")
+  barplot(result$Emissions, names.arg = result$Year, beside = TRUE, col= c("red", "green", "black", "yellow"), legend = levels(unique(result$type)),  xlab = "years", ylab = "Emission", main = "Fine Particular Matter Air Pollutant in Baltimore City, Maryland by Years")
   #mandatory housekeeping 
-  #dev.off()
+  dev.off()
 }
